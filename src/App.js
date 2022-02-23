00c6ed11e927b3719ac9bd1261f337b5ad88fde8
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-02-23 18:03:18 +0900

    [Feat] 비밀번호 찾기 아이디찾기 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header"
+import FindId from "./Page/FindId";
+import FindPw from "./Page/FindPw";
 import List from "./Page/List";
 import Login from "./Page/Login";
 import Mypage from "./Page/Mypage";
@@ -16,6 +18,8 @@ function App() {
         <Route path="/mypage" element={<Mypage />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/signup" element={<Signup />}/>
+        <Route path="/find_id" element={<FindId />}/>
+        <Route path="/find_password" element={<FindPw />}/>
       </Routes>
     </div>
   );
