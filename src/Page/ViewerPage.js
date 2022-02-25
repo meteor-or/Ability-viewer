3c30f189b0b9916b0be7cd51992f258841d28e0b
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-02-25 17:54:56 +0900

    [Feat] hashtag 생성 로직 구현
    -  textarea에서 #해시태그 style을 바꾸는 건 어려움  : 대신 두 가지 로직 만듬
    - 1. 본문에 #을 적으면 해시태그로 인식
    : textarea에서는 #이 보이지만 전체보기로 간다면 사라지도록 하기
    - 2. 따로 해시태그용 input을  생성 그곳에 적고 띄어쓰기하면 해시태그로 인식

--- src/Page/ViewerPage.js
+++ src/Page/ViewerPage.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import Hashtag from '../components/Hashtag';
 
 const ViewerPage = () => {
+	
+	const [hashtags, setHashtags] = useState([]);
+	const [임시, set임시] = useState()
+	const [hashtags2, setHashtags2] = useState([]);
+	
+	const onChangeHashtag = (e) =>{
+		setHashtags(e.target.value.match(/#[^\s#]+/g))
+	}
+	const onChangeHashtag2 = (e) => {
+		set임시(e.target.value.trim().split(" "))
+	}
+	const clickEnter = useCallback((e) =>{
+		if (e.keyCode == 32 && e.target.value.trim() !== '') {
+			console.log("임시",임시)
+			setHashtags2((hashtags2) => [...hashtags2, 임시])
+			e.target.value = ""
+		}
+	})
 	return (
-		<div>ViewerPage</div>
+		<div className='px-24'>
+			<div className='pt-20'></div>
+			<div className='bg-white flex f-column b-400 br-12 py-12 px-16'>
+				<input className='unset py-8' placeholder='제목을 입력하세요' style={{borderBottom: "1px solid var(--gray--300)"}}></input>
+				<div className='hashtagWrap flex f-ai-center' style={{borderBottom: "1px solid var(--gray--300)"}} >
+					<div className='hastagBox'>
+						{hashtags2 && hashtags2.map((text) => (
+							<Hashtag text={text}/>
+						))}
+					</div>
+					<input className='unset py-8 ' placeholder='해시태그를 입력하세요' onKeyUp={clickEnter} onChange={onChangeHashtag2}/>
+				</div>
+				<div className='hashtagList flex'>
+					{hashtags && hashtags.map((text) => (
+							<Hashtag text={text}/>)
+						)}
+				</div>
+				<textarea className='unset border-box py-8' onChange={onChangeHashtag} placeholder='내용을 입력하세요'>
+				</textarea>
+			</div>
+		</div>
 	)
 }
 
