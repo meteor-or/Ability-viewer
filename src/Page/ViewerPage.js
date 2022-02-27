28e63fd0b47793e48c9acd2bc719181ac6c459a8
meteor lee <82145837+meteor-or@users.noreply.github.com>
2022-02-27 10:02:36 +0900

    [Fix] 로직 선정후 페이지 정리

--- src/Page/ViewerPage.js
+++ src/Page/ViewerPage.js
@@ -1,25 +1,21 @@
 import React, { useCallback, useState } from 'react'
 import Hashtag from '../components/Hashtag';
+import { observer } from 'mobx-react';
 
 const ViewerPage = () => {
-	
+	const [inputHashtagVal, setInputHashtagVal] = useState()
 	const [hashtags, setHashtags] = useState([]);
-	const [임시, set임시] = useState()
-	const [hashtags2, setHashtags2] = useState([]);
 	
-	const onChangeHashtag = (e) =>{
-		setHashtags(e.target.value.match(/#[^\s#]+/g))
+	const onChangeHashtag = (e) => {
+		setInputHashtagVal(e.target.value.trim().split(" "))
 	}
-	const onChangeHashtag2 = (e) => {
-		set임시(e.target.value.trim().split(" "))
-	}
-	const clickEnter = useCallback((e) =>{
+	const clickSpace = ((e) =>{
 		if (e.keyCode == 32 && e.target.value.trim() !== '') {
-			console.log("임시",임시)
-			setHashtags2((hashtags2) => [...hashtags2, 임시])
+			setHashtags((hashtags) => [...hashtags, inputHashtagVal])
 			e.target.value = ""
 		}
 	})
+
 	return (
 		<div className='px-24'>
 			<div className='pt-20'></div>
@@ -27,22 +23,17 @@ const ViewerPage = () => {
 				<input className='unset py-8' placeholder='제목을 입력하세요' style={{borderBottom: "1px solid var(--gray--300)"}}></input>
 				<div className='hashtagWrap flex f-ai-center' style={{borderBottom: "1px solid var(--gray--300)"}} >
 					<div className='hastagBox'>
-						{hashtags2 && hashtags2.map((text) => (
+						{hashtags && hashtags.map((text) => (
 							<Hashtag text={text}/>
 						))}
 					</div>
-					<input className='unset py-8 ' placeholder='해시태그를 입력하세요' onKeyUp={clickEnter} onChange={onChangeHashtag2}/>
+					<input className='unset py-8 ' placeholder='해시태그를 입력하세요' onKeyUp={clickSpace} onChange={onChangeHashtag}/>
 				</div>
-				<div className='hashtagList flex'>
-					{hashtags && hashtags.map((text) => (
-							<Hashtag text={text}/>)
-						)}
-				</div>
-				<textarea className='unset border-box py-8' onChange={onChangeHashtag} placeholder='내용을 입력하세요'>
+				<textarea className='unset border-box py-8' placeholder='내용을 입력하세요'>
 				</textarea>
 			</div>
 		</div>
 	)
 }
 
-export default ViewerPage
+export default observer(ViewerPage);
--- src/components/Hashtag.js
+++ src/components/Hashtag.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hashtag = (props) => {
 	return (
-		<span className='bg-400 w-fit px-8 py-4 ml-4 tc-white br-8'>
+		<span className='bg-400 w-fit px-8 py-4 mx-4 tc-white br-8'>
 			#{props.text}
 		</span>
 	)
