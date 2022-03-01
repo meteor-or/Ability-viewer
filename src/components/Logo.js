f6d64f33b37fef893c4a005dd1931a10183ffde5
meteor lee <82145837+meteor-or@users.noreply.github.com>
2022-03-01 19:04:59 +0900

    [Chore] logo 주석 추가

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom'
 import logoShort from '../asset/icon.png'
 import logoFull from '../asset/logo.png'
 
-
+// (A) 이니셜 한 글자만 
 export const LogoShort = () => {
 	return (
-		<Link to="/">
-			<img src={logoShort} className="w-full h-full" />
+		<Link to='/'>
+			<img src={logoShort} className='w-full h-full' alt='logo'/>
 		</Link>
 	)
 }
+
+// (Ability) 전체 
 export const LogoFull = () => {
 	return (
-		<Link to="/">
-			<img src={logoFull} className="w-fit h-fit" />
+		<Link to='/'>
+			<img src={logoFull} className='w-fit h-fit' alt='logo'/>
 		</Link>
 	)
 }
