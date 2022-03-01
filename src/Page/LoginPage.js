24f051c388298e029698d229979100af85fe2cdb
meteor lee <82145837+meteor-or@users.noreply.github.com>
2022-03-01 19:11:07 +0900

    [Feat] login 기능 구현
    -Input component 사용시 value값을 끌어올릴 수 없어서 삭제 (submit도 마찬가지)

--- src/Page/LoginPage.js
+++ src/Page/LoginPage.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Submit from '../components/Button/Submit'
-import TextInput from '../components/Input/TextInput'
-import PasswordInput from '../components/Input/PasswordInput'
+import axios from 'axios';
 
 const LoginPage = () => {
+	const [loginId, setLoginId] = useState("")
+	const [loginPw, setLoginPw] = useState("")
+	
+	const handlelogin = async() => {
+		const request = {
+			"id" : loginId,
+			"password" : loginPw
+		}
+		try {
+			await axios({
+				method: "post",
+				url: "https://ability-backend.azurewebsites.net/login",
+				data: request,
+			});
+		} catch(error) {
+			console.log(error.response.data)
+		}
+	}
+	
 	return (
 		<div id='loginPage'>
-			<div className='px-24 w-full flex f-column f-ai-center'>
+			<div className='px-24 py-24 w-full flex f-column f-ai-center'>
 				<from method="post" id="authForm">
-					<TextInput id="IoginId" placeholder="아이디"/>
-					<PasswordInput id="IoginPw" placeholder="비밀번호"/>
-					<Submit type='submit' value="로그인" />
+					<label htmlFor="IoginId">아이디</label>
+					<input type="text" 
+					id="loginId"
+					className='unset border-box w-full bg-white br-8 px-16 py-12 b-400 my-8 active-b-800 pretendard fs-16' 
+					placeholder="아이디를 입력하세요" 
+					onChange={(e)=>{setLoginId(e.target.value)}}
+					required>
+					</input>
+
+					<label htmlFor="pw">비밀번호</label>
+					<input type="password" 
+					id="loginPw"
+					className='unset border-box w-full bg-white br-8 px-16 py-12 b-400 my-8 active-b-800 pretendard fs-16' 
+					placeholder="비밀번호를 입력하세요"
+					onChange={(e)=>{setLoginPw(e.target.value)}}>
+					</input>
+
+					<button 
+					className='unset border-box br-12 b-500 bg-500 tc-50 w-full px-16 py-12 my-8' 
+					style={{textAlign: 'center', cursor: 'pointer'}}
+					onClick={handlelogin}>로그인</button>
 				</from>
+				<div className='errorBox'></div>
 				<ul className='flex'>
-					<li><Link to="/signup">회원가입 </Link></li>
-					<li><Link to="/find_id">아이디찾기 </Link></li>
+					<li className='mr-4'><Link to="/signup">회원가입 |</Link></li>
+					<li className='mr-4'><Link to="/find_id">아이디찾기 |</Link></li>
 					<li><Link to="/find_password">비밀번호찾기 </Link></li>
 				</ul>
 			</div>
