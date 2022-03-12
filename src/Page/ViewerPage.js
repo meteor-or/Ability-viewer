081410806230a6c41b733d41c94d3ab42186c644
meteor-or <metemon59@gmail.com>
2022-03-12 17:54:22 +0900

    [Fix] 해쉬태그 삭제시 생기는 오류
    -style : 해시태그 색 대비 접근성 문제 해결

--- src/Page/ViewerPage.js
+++ src/Page/ViewerPage.js
@@ -26,20 +26,27 @@ const ViewerPage = () => {
 
 	const [inputHashtagVal, setInputHashtagVal] = useState()
 	const [hashtags, setHashtags] = useState([]);
-	
 	const onChangeHashtag = (e) => {
 		setInputHashtagVal(e.target.value.trim().split(" "))
 	}
-	const clickSpace = ((e) =>{
-		if (e.keyCode == 32 && e.target.value.trim() !== '') {
-			setHashtags((hashtags) => [...hashtags, inputHashtagVal])
-			e.target.value = ""
+	const deleteLastHashtag = () => {
+		setHashtags(hashtags =>[...hashtags, hashtags.pop()])
+		hashtags && document.querySelector(".hastagBox").lastChild.remove()
+	}
+	const addHashtag = (e) => {
+		setHashtags((hashtags) => [...hashtags, inputHashtagVal])
+		e.target.value = ""
+	} 
+	const keyEvent = ((e) =>{
+		if ((e.keyCode == 32) && e.target.value.trim() !== '') {
+			addHashtag(e)
 		}
-		if (e.keyCode == 8 && hashtags) {
-			hashtags.pop()
-			setHashtags(hashtags)
-			const lastHashtag = document.querySelector(".hastagBox").lastChild
-			lastHashtag.remove()
+		if ((e.keyCode == 13) && e.target.value.trim() !== '') {
+			addHashtag(e)
+		}
+		if (e.target.value == "" && e.keyCode == 8 ) {
+			e.preventDefault()
+			deleteLastHashtag()
 		}
 	})
 	
@@ -91,16 +98,16 @@ const ViewerPage = () => {
 			</div>
 			<div className='Viewer'>
 				<div className='bg-white flex f-column br-12 py-12 px-24'>
-					<input className='unset py-8' placeholder='제목을 입력하세요' style={{borderBottom: "1px solid var(--gray--300)"}}></input>
-					<div className='hashtagWrap flex f-ai-center' style={{borderBottom: "1px solid var(--gray--300)"}} >
-						<div className='hastagBox'>
+					<input className='unset py-12' placeholder='제목을 입력하세요' style={{borderBottom: "1px solid var(--gray--300)"}}></input>
+					<div className='hashtagWrap flex f-column' style={{borderBottom: "1px solid var(--gray--300)"}} >
+						<div className='hastagBox my-4 flex f-wrap'>
 							{hashtags && hashtags.map((text, index) => (
 								<Hashtag key={index} text={text}/>
 							))}
 						</div>
-						<input className='unset py-8 ' placeholder='해시태그를 입력하세요' onKeyUp={clickSpace} onChange={onChangeHashtag}/>
+						<input className='unset py-8 mb-4' placeholder='해시태그를 입력하세요' onKeyUp={keyEvent} onChange={onChangeHashtag}/>
 					</div>
-					<textarea className='unset border-box py-8 user_text'defaultValue={optionStore.text} style={{height: 500}} placeholder='내용을 입력하세요' />
+					<textarea className='unset border-box py-12 user_text'defaultValue={optionStore.text} style={{height: 500}} placeholder='내용을 입력하세요' />
 				</div>
 			</div>
 		</div>
--- src/components/Hashtag.js
+++ src/components/Hashtag.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Hashtag = (props) => {
 	return (
-		<span className='hashtag bg-400 w-fit px-8 py-4 mx-4 tc-white br-8'>
-			#{props.text}
-		</span>
+		<div className='fs-14 hashtag bg-100 w-fit py-4 px-8 mr-4 mt-4 tc-600 br-4' style={{cursor:'pointer'}}>
+			{props.text}
+		</div>
 	)
 }
 
